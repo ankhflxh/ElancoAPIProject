fbef63de31c07c97773de93d55b70d44b1b156cf
unknown <ashleyjonathan353.com>
2024-11-24 10:45:20 +0000

    final commit I hope

--- elanco.js
+++ elanco.js
@@ -1,15 +1,18 @@
+// I used strict mode here to catch common errors
 "use strict";
+
+// Selecting the search input field and the container where country data will be displayed.
 const searchIn = document.querySelector(".search");
 const countriesContainer = document.querySelector(".countries");
 
+// Function to fetch data from the API with error handling.
 const fetchData = async (url, options) => {
   try {
+    // Fetching data from the API and Logging the response for debugging purposes and then Parsing the response as JSON.
     const response = await fetch(url, options);
-
     console.log({ response });
-
     const responseData = await response.json();
-
+    // Returning the response data if the response is successful, otherwise throwing an error.
     if (response?.ok) {
       return responseData;
     } else {
@@ -20,28 +23,34 @@ const fetchData = async (url, options) => {
     return error;
   }
 };
+
+// Fetching general information about countries like currency, flag, capital, etc and population data for countries
 const countriesData = fetchData(
   `https://countriesnow.space/api/v0.1/countries/info?returns=currency,flag,cities,capital`
 );
 const countriesPopulation = fetchData(
   `https://countriesnow.space/api/v0.1/countries/population`
 );
-
 console.log({ countriesData });
 
+// Function to format large numbers
 const formatter = (number) =>
   Intl.NumberFormat({
     maximumSignificantDigits: 3,
   }).format(number);
 
+// Function to generate and display country data on the page.
 const renderCountry = async (country) => {
+  // Waiting for population data to be fetched, then displaying it for the specific country.
   countriesPopulation.then((data) => {
     const population = data?.data
       .find((item) => {
+        // Matching country names to find the correct population data and  the latest population count.
         return country?.name?.toLowerCase() === item?.country.toLowerCase();
       })
       ?.populationCounts.slice(-1)[0].value;
 
+    // Generating the HTML to display country information.
     const html = ` 
         <div class="country">
             <img class="country__img" src="${country.flag}" />
@@ -60,10 +69,14 @@ const renderCountry = async (country) => {
             </div>
         </div>
         `;
+
+    // Inserting the generated HTML into the countries container on the page.
     countriesContainer.insertAdjacentHTML("beforeend", html);
     console.log({ population });
   });
 };
+
+// Function to display an error message if something goes wrong.
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
 };
@@ -89,10 +102,12 @@ countriesData
     renderError(err);
   });
 
+// Setting up an event listener for the search input field, to filter countries based on the query.
 searchIn.addEventListener("input", (e) => {
   const searchItem = e.target.value.toLowerCase();
   countriesContainer.innerHTML = "";
 
+  // Filtering through the countries list and displaying only those that match the search query.
   allCountries
     .filter((country) => country.name.toLowerCase().includes(searchItem))
     .forEach((filteredCountry) => {
